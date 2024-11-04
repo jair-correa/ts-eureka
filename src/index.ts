154b73c425b256f68c0e68f2aae26af502a63895
jair-correa <jair-correa@outlook.com.br>
2024-11-04 20:58:46 -0300

    feat: express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log("listening on port %d!", port));
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
